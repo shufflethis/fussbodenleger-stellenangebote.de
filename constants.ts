9ee01139ceafa5a45707da080baf17ab2ec5cd37
Autoblogger <dev@famefact.de>
2026-03-04 11:05:42 +0100

    chore: update job listings from external sources (HR-JobScraper)
    25 jobs injected from: Arbeitsagentur, Indeed, StepStone, meinestadt.de

--- constants.ts
+++ constants.ts
@@ -3,52 +3,227 @@ import { JobListing } from './types';
 export const JOB_LISTINGS: JobListing[] = [
   {
     id: '1',
-    title: 'Parkettleger (m/w/d) für Luxus-Immobilien',
-    company: 'EdelBoden GmbH',
-    location: 'München, Bayern',
+    title: 'Bodenleger (w/m/d)',
+    company: 'Farbe u.Raum Malerwerkstatt GmbH',
+    location: 'Schwerin, Mecklenburg',
     type: 'Vollzeit',
-    salary: 'ab 3.800€',
-    description: 'Wir suchen einen erfahrenen Parkettleger für hochwertige Verlegearbeiten (Fischgrät, Tafelparkett) im Raum München.',
-    postedDate: 'Vor 2 Tagen'
+    description: 'Bodenleger (w/m/d)',
+    postedDate: '2026-03-03'
   },
   {
     id: '2',
-    title: 'Bodenleger / Allrounder für Sanierung',
-    company: 'BauTeam Nord',
-    location: 'Hamburg',
+    title: 'Fußbodenleger, Maler, Trockenbauer',
+    company: 'Holger Behrend Ausbaugewerbe',
+    location: 'Magdeburg',
     type: 'Vollzeit',
-    salary: '22,50€ / Std.',
-    description: 'Vorbereitung von Untergründen, Verlegung von Vinyl und Teppich. Firmenwagen wird gestellt.',
-    postedDate: 'Gestern'
+    description: 'Fußbodenleger, Maler, Trockenbauer',
+    postedDate: '2026-03-03'
   },
   {
     id: '3',
-    title: 'Selbstständiger Subunternehmer gesucht',
-    company: 'Objektbau Schmidt',
-    location: 'Frankfurt am Main',
-    type: 'Projektbasis',
-    salary: 'Nach Aufmaß',
-    description: 'Wir vergeben 2000m² Teppichverlegung in einem Bürokomplex. Start sofort möglich.',
-    postedDate: 'Vor 5 Stunden'
+    title: 'Bodenleger (m/w/d)',
+    company: 'Zeitlimit Personaldienstleistungen GmbH',
+    location: 'Kiel',
+    type: 'Vollzeit',
+    description: 'Bodenleger (m/w/d)',
+    postedDate: '2026-03-03'
   },
   {
     id: '4',
-    title: 'Ausbildung zum Bodenleger 2024',
-    company: 'Meisterbetrieb Müller',
-    location: 'Berlin',
+    title: 'Bodenleger (m/w/d)',
+    company: 'Orizon GmbH, Niederlassung Trier',
+    location: 'Konz',
     type: 'Vollzeit',
-    salary: 'Ausbildungsvergütung',
-    description: 'Werde Teil unseres Teams und lerne das Handwerk von der Pike auf. Übernahmegarantie bei guten Leistungen.',
-    postedDate: 'Vor 1 Woche'
+    description: 'Bodenleger (m/w/d)',
+    postedDate: '2026-03-03'
   },
-   {
+  {
     id: '5',
-    title: 'Estrichleger (m/w/d) gesucht',
-    company: 'Beton & Boden AG',
-    location: 'Köln / Bonn',
+    title: 'Allround-Handwerker / Bauhelfer, Maurer, Maler, Fliesenleger, Bodenleger (m/w/d) gesucht',
+    company: 'Bau & Informationstechnik Müller Maik',
+    location: 'Magdeburg',
     type: 'Vollzeit',
-    salary: 'Tarif + Zulagen',
-    description: 'Bedienung von Estrichpumpen, Glätten und Nachbehandeln. Montagebereitschaft erwünscht.',
-    postedDate: 'Vor 3 Tagen'
+    description: 'Allround-Handwerker / Bauhelfer, Maurer, Maler, Fliesenleger, Bodenleger (m/w/d) gesucht',
+    postedDate: '2026-02-27'
+  },
+  {
+    id: '6',
+    title: 'Handwerker / Hausmeister (m/w/d) für Hausverwaltung gesucht',
+    company: 'Immobilien-Verwaltungsgesellsc haft Burg',
+    location: 'Burg bei Magdeburg',
+    type: 'Vollzeit',
+    description: 'Handwerker / Hausmeister (m/w/d) für Hausverwaltung gesucht',
+    postedDate: '2026-02-05'
+  },
+  {
+    id: '7',
+    title: 'Fußbodenleger/in (m/w/d)',
+    company: 'Hausgeister GmbH',
+    location: 'Chemnitz, Sachsen',
+    type: 'Vollzeit',
+    description: 'Fußbodenleger/in (m/w/d)',
+    postedDate: '2026-02-05'
+  },
+  {
+    id: '8',
+    title: 'Maler- und Fußbodenleger (m/w/d)',
+    company: 'Gebäude- und Hygiene-Service Halle GmbH & Co. KG',
+    location: 'Halle (Saale)',
+    type: 'Vollzeit',
+    description: 'Maler- und Fußbodenleger (m/w/d)',
+    postedDate: '2026-01-30'
+  },
+  {
+    id: '9',
+    title: 'Klebepraktiker / Fußbodenleger (m/w/d)',
+    company: 'expertum GmbH',
+    location: 'Salzgitter',
+    type: 'Vollzeit',
+    description: 'Klebepraktiker / Fußbodenleger (m/w/d)',
+    postedDate: '2026-01-28'
+  },
+  {
+    id: '10',
+    title: 'Fußbodenleger (m/w/d)',
+    company: 'I.K. Hofmann GmbH',
+    location: 'Markkleeberg',
+    type: 'Vollzeit',
+    description: 'Fußbodenleger (m/w/d)',
+    postedDate: '2026-01-21'
+  },
+  {
+    id: '11',
+    title: 'Pakett- oder Fußbodenleger (m/w/d)',
+    company: 'Otto Kast',
+    location: 'Witten',
+    type: 'Vollzeit',
+    description: 'Pakett- oder Fußbodenleger (m/w/d)',
+    postedDate: '2025-12-11'
+  },
+  {
+    id: '12',
+    title: 'Fußbodenleger/in (m/w/d) gesucht!',
+    company: 'Epic Bau GmbH',
+    location: 'Leipzig',
+    type: 'Vollzeit',
+    description: 'Fußbodenleger/in (m/w/d) gesucht!',
+    postedDate: '2025-12-08'
+  },
+  {
+    id: '13',
+    title: 'Fussbodenleger  (mIwId) - HEILBRONN   -',
+    company: 'Jobdistrict GmbH',
+    location: 'Heilbronn, Neckar',
+    type: 'Vollzeit',
+    description: 'Fussbodenleger  (mIwId) - HEILBRONN   -',
+    postedDate: '2025-12-05'
+  },
+  {
+    id: '14',
+    title: 'Bodenleger',
+    company: 'Uwe Gawande',
+    location: 'Dresden',
+    type: 'Vollzeit',
+    description: 'Bodenleger',
+    postedDate: '2025-10-21'
+  },
+  {
+    id: '15',
+    title: 'Kraftfahrer (m/w/d)',
+    company: 'Container Vermietung u.Verkauf Menzl GmbH',
+    location: 'Lichtenau bei Chemnitz, Sachsen',
+    type: 'Vollzeit',
+    description: 'Kraftfahrer (m/w/d)',
+    postedDate: '2025-10-14'
+  },
+  {
+    id: '16',
+    title: 'Maler/in, Lackier/in, Tapezierer/in, Fußbodenleger/in (m/w/d) gesucht',
+    company: 'Gebäudegesellschaft Limbach- Oberfrohna mbH',
+    location: 'Limbach-Oberfrohna',
+    type: 'Vollzeit',
+    description: 'Maler/in, Lackier/in, Tapezierer/in, Fußbodenleger/in (m/w/d) gesucht',
+    postedDate: '2025-10-09'
+  },
+  {
+    id: '17',
+    title: 'Maler u. Lackierer oder Fußbodenleger (m/w/d) - auch Quereinstieg möglich',
+    company: 'Thomas Müller Projektmanagment GmbH',
+    location: 'Schwarzenberg/Erzgebirge',
+    type: 'Vollzeit',
+    description: 'Maler u. Lackierer oder Fußbodenleger (m/w/d) - auch Quereinstieg möglich',
+    postedDate: '2025-09-02'
+  },
+  {
+    id: '18',
+    title: 'Fussbodenleger  (mIwId) -Dortmund   -',
+    company: 'Jobdistrict GmbH',
+    location: 'Dortmund',
+    type: 'Vollzeit',
+    description: 'Fussbodenleger  (mIwId) -Dortmund   -',
+    postedDate: '2025-08-28'
+  },
+  {
+    id: '19',
+    title: 'Fussbodenleger  (mIwId) - Kassel   -',
+    company: 'Jobdistrict GmbH',
+    location: 'Kassel, Hessen',
+    type: 'Vollzeit',
+    description: 'Fussbodenleger  (mIwId) - Kassel   -',
+    postedDate: '2025-08-28'
+  },
+  {
+    id: '20',
+    title: 'Fussbodenleger  (mIwId) -Schwerin -',
+    company: 'Jobdistrict GmbH',
+    location: 'Schwerin, Mecklenburg',
+    type: 'Vollzeit',
+    description: 'Fussbodenleger  (mIwId) -Schwerin -',
+    postedDate: '2025-08-26'
+  },
+  {
+    id: '21',
+    title: 'Fussbodenleger  (mIwId) - Kassel  -',
+    company: 'Jobdistrict GmbH',
+    location: 'Kassel, Hessen',
+    type: 'Vollzeit',
+    description: 'Fussbodenleger  (mIwId) - Kassel  -',
+    postedDate: '2025-08-26'
+  },
+  {
+    id: '22',
+    title: 'Fußbodenleger (m/w/d)',
+    company: 'Biermanns- Raumdesign GmbH',
+    location: 'Bremen',
+    type: 'Vollzeit',
+    description: 'Fußbodenleger (m/w/d)',
+    postedDate: '2025-08-19'
+  },
+  {
+    id: '23',
+    title: 'Fussbodenleger  (mIwId) - Dresden    -',
+    company: 'Jobdistrict GmbH',
+    location: 'Dresden',
+    type: 'Vollzeit',
+    description: 'Fussbodenleger  (mIwId) - Dresden    -',
+    postedDate: '2025-06-26'
+  },
+  {
+    id: '24',
+    title: 'Fussbodenleger  (mIwId) - Bremen    -',
+    company: 'Jobdistrict GmbH',
+    location: 'Bremen',
+    type: 'Vollzeit',
+    description: 'Fussbodenleger  (mIwId) - Bremen    -',
+    postedDate: '2025-06-26'
+  },
+  {
+    id: '25',
+    title: 'Fussbodenleger  (mIwId) - LÜBECK   -',
+    company: 'Jobdistrict GmbH',
+    location: 'Lübeck',
+    type: 'Vollzeit',
+    description: 'Fussbodenleger  (mIwId) - LÜBECK   -',
+    postedDate: '2025-06-02'
   }
 ];
